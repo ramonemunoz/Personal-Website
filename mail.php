c8c89c6bd9f12a5ecdd798d13bccec2dd0b7f598
ramonemunoz <ramonemunoz@users.noreply.github.com>
2015-03-01 15:41:27 -0800

    added the contact form

---
+++ mail.php
@@ -0,0 +1,11 @@
+<?php
+$name = $_POST['name'];
+$email = $_POST['email'];
+$subject = $_POST['subject'];
+$message = $_POST['message'];
+$formcontent=" From: $name \n subject: $subject \n  Message: $message";
+$recipient = "ramonmunoz1027@gmail.com";
+$mailheader = "From: $email \r\n";
+mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
+echo "Thank You!" . " -" . "<a href='index.html' style='text-decoration:none;color:#ff0099;'> Return Home</a>";
+?>
